ad851560ae1dab6e0c63b63e1e58d377d90e0067
Araujo Brenner <b.araujo@reply.com>
2019-11-12 15:20:49 -0300

    create questions

--- 3.php
+++ 3.php
@@ -1,11 +1,62 @@
 <?php
-    $soma =0;
-    for ($i=1 ; $i<1000; $i++)
+class Numero 
+{
+    public $numero1;
+    public $numero2;
+    public $numero3;
+
+  
+    public function getNumero1() 
     {
-        if ($i%3==0 || $i%5==0)
-        {
-            $soma+=$i;
-        }
+        return $this->numero1;
     }
-    echo $soma;
+  
+    public function setNumero1($numero1) 
+    {
+        $this->numero1= $numero1;
+    }
+  
+    public function getNumero2() 
+    {
+        return $this->numero1;
+    }
+  
+    public function setNumero2($numero2) 
+    {
+        $this->numero2= $numero2;
+    }
+
+    public function getNumero3() 
+    {
+        return $this->numero1;
+    }
+  
+    public function setNumero3($numero3) 
+    {
+        $this->numero3= $numero3;
+    }
+
+    public function multiplicacao()
+    {
+        return $this->numero1 * $this->numero2 * $this->numero3;
+    }
+    public function soma()
+    {
+        return $this->numero1 + $this->numero2 + $this->numero3;
+    }
+    public function subtracao()
+    {
+        return $this->numero1 - $this->numero2 - $this->numero3;
+    }
+  
+}
+    ##Exemplo de uso
+    $numeros = new Numero;
+    $numeros->setNumero1(2);
+    $numeros->setNumero2(3);
+    $numeros->setNumero3(5);
+
+    echo ($numeros->multiplicacao());
+
+
 ?>
